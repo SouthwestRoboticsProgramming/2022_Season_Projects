1995c1bf856980dbb70e10502cd4104621bb975c
Ryan Heuer <63077980+rmheuer@users.noreply.github.com>
2021-10-26 13:00:09 -0500

    Added label and buttons to task list for TaskManager Visualizer

--- TaskManager/Controller/src/frc/taskmanager/controller/TaskViewPanel.java
+++ TaskManager/Controller/src/frc/taskmanager/controller/TaskViewPanel.java
@@ -9,25 +9,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TaskViewPanel extends JPanel {
-    private final JScrollPane listScrollPane;
-    private final JList<String> taskList;
+    private final Coprocessor cp;
+    private final DefaultListModel<String> listModel;
 
     public TaskViewPanel(Coprocessor cp) {
         super();
+        this.cp = cp;
 
-        setLayout(new BorderLayout());
+        setLayout(new BorderLayout(3, 3));
 
-        // Initialize and add a list
-        DefaultListModel<String> listModel = new DefaultListModel<>();
-        taskList = new JList<>(listModel);
+        // Set up task list panel
+        JPanel taskListPanel = new JPanel();
+        taskListPanel.setLayout(new BorderLayout(3, 3));
+        {
+            Dimension size = new Dimension(200, -1);
 
-        listScrollPane = new JScrollPane(taskList);
-        add(listScrollPane, BorderLayout.WEST);
+            // Set up the task list header
+            JPanel listHeader = new JPanel();
+            listHeader.setLayout(new BorderLayout(3, 3));
+            {
+                JLabel label = new JLabel("Tasks");
+                listHeader.add(label, BorderLayout.CENTER);
+            }
+            taskListPanel.add(listHeader, BorderLayout.NORTH);
 
-        Dimension listSize = new Dimension(200, -1);
-        listScrollPane.setMinimumSize(listSize);
-        listScrollPane.setPreferredSize(listSize);
-        listScrollPane.setMaximumSize(listSize);
+            // Set up the task list
+            listModel = new DefaultListModel<>();
+            JList<String> taskList = new JList<>(listModel);
+            JScrollPane listScrollPane = new JScrollPane(taskList);
+            listScrollPane.setMinimumSize(size);
+            listScrollPane.setPreferredSize(size);
+            listScrollPane.setMaximumSize(size);
+            taskListPanel.add(listScrollPane, BorderLayout.CENTER);
+
+            // Set up the task list footer
+            JPanel listFooter = new JPanel();
+            listFooter.setLayout(new BorderLayout(3, 3));
+            {
+                JButton addButton = new JButton("Add New");
+                JButton removeButton = new JButton("Remove");
+
+                listFooter.add(addButton, BorderLayout.WEST);
+                listFooter.add(removeButton, BorderLayout.EAST);
+            }
+            taskListPanel.add(listFooter, BorderLayout.SOUTH);
+        }
+
+        // Add the elements to this
+        add(taskListPanel, BorderLayout.WEST);
+
+        refresh();
+    }
+
+    private void refresh() {
+        // Clear the list
+        listModel.clear();
 
         // Get all of the tasks from the coprocessor and put them into the list
         cp.getAllTasks().thenAccept((taskSet) -> {
