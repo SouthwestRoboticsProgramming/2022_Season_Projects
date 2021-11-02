ca359de6b367d7b5587e50fdd2ce6277b7825ba4
Ryan Heuer <63077980+rmheuer@users.noreply.github.com>
2021-11-02 07:48:22 -0500

    Added looking for lidar

---
+++ Lidar/src/main/java/frc/LidarMain.java
@@ -0,0 +1,24 @@
+package frc;
+
+import com.fazecast.jSerialComm.SerialPort;
+
+public class LidarMain {
+    public static void main(String[] args) {
+        SerialPort lidarPort = null;
+        System.out.println("Available serial ports:");
+        for (SerialPort port : SerialPort.getCommPorts()) {
+            System.out.println(port.getSystemPortName() + " [" + port.getDescriptivePortName() + "]: " + port.getPortDescription());
+
+            if (port.getPortDescription().contains("CP2102")) {
+                if (lidarPort != null) {
+                    throw new RuntimeException("Port conflict, are multiple lidars connected?");
+                }
+                lidarPort = port;
+            }
+        }
+        if (lidarPort == null) {
+            throw new RuntimeException("Could not find lidar");
+        }
+        System.out.println("Found lidar on port " + lidarPort.getSystemPortName());
+    }
+}
