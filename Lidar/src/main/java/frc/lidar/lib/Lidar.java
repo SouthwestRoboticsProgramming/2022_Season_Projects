c48aea73c30cb0325dda8e55ae5fc2a1f40e87c3
Ryan Heuer <63077980+rmheuer@users.noreply.github.com>
2021-11-03 10:16:16 -0500

    Basic lidar communication

--- Lidar/src/main/java/frc/lidar/lib/Lidar.java
+++ Lidar/src/main/java/frc/lidar/lib/Lidar.java
@@ -1,6 +1,8 @@
 package frc.lidar.lib;
 
 import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
 import com.fazecast.jSerialComm.SerialPortInvalidPortException;
 import frc.lidar.lib.exceptions.InvalidLidarPortException;
 import frc.lidar.lib.exceptions.LidarBusyException;
@@ -16,8 +18,27 @@ import java.util.function.Consumer;
  *
  * @author rmheuer
  */
-public class Lidar {
+public class Lidar implements SerialPortDataListener {
     private static final String MODEL_NUM = "CP2102";
+
+    // Start flags
+    private static final byte START_FLAG = (byte) 0xA5;
+    private static final byte START_FLAG_2 = 0x5A;
+
+    // Request id constants
+    private static final byte REQUEST_STOP = 0x25;
+    private static final byte REQUEST_RESET = 0x40;
+    private static final byte REQUEST_SCAN = 0x20;
+    private static final byte REQUEST_GET_INFO = 0x50;
+    private static final byte REQUEST_GET_HEALTH = 0x52;
+
+    private enum ResponseType {
+        INFO, HEALTH, SCAN
+    }
+
+    private final SerialPort port;
+    private final Set<CompletableFuture<LidarInfo>> infoFutures;
+    private final Set<CompletableFuture<LidarHealth>> healthFutures;
 
     /**
      * Gets a list of serial ports that could be a lidar.
@@ -72,6 +93,20 @@ public class Lidar {
      */
     public Lidar(SerialPort port) {
         checkPort(port);
+        System.out.println("Starting lidar on port " + port.getSystemPortName());
+        this.port = port;
+
+        infoFutures = new HashSet<>();
+        healthFutures = new HashSet<>();
+
+        port.setBaudRate(115200);
+        port.setParity(SerialPort.NO_PARITY);
+        port.setNumStopBits(1);
+        port.setNumDataBits(8);
+        if (!port.openPort()) {
+            throw new RuntimeException("Failed to connect");
+        }
+        port.addDataListener(this);
     }
 
     /**
@@ -82,7 +117,12 @@ public class Lidar {
      * @return future for device info
      */
     public CompletableFuture<LidarInfo> getInfo() {
-        return null;
+        CompletableFuture<LidarInfo> future = new CompletableFuture<>();
+        infoFutures.add(future);
+
+        sendRequest(REQUEST_GET_INFO, null);
+
+        return future;
     }
 
     /**
@@ -94,7 +134,12 @@ public class Lidar {
      * @return future for device health
      */
     public CompletableFuture<LidarHealth> getHealth() {
-        return null;
+        CompletableFuture<LidarHealth> future = new CompletableFuture<>();
+        healthFutures.add(future);
+
+        sendRequest(REQUEST_GET_HEALTH, null);
+
+        return future;
     }
 
     /**
@@ -104,7 +149,8 @@ public class Lidar {
      * @throws LidarBusyException if busy
      */
     public void startScanning() {
-
+        port.clearDTR();
+        sendRequest(REQUEST_SCAN, null);
     }
 
     /**
@@ -145,6 +191,14 @@ public class Lidar {
         return false;
     }
 
+    /**
+     * Closes the connection to the lidar. No other methods should
+     * be called on this class after closing.
+     */
+    public void close() {
+        port.closePort();
+    }
+
     // Finds a serial port connected to a lidar
     private static SerialPort getDefaultPort() {
         Set<SerialPort> ports = findLidars();
@@ -173,5 +227,67 @@ public class Lidar {
         if (!port.getPortDescription().contains(MODEL_NUM)) {
             throw new InvalidLidarPortException("Device on port '" + port.getSystemPortName() + "' is not a lidar");
         }
+    }
+
+    // Sends a request with optional payload (no payload if null)
+    // Request format:
+    //     [Start flag] [Command] | [Payload Size] [Payload Data...] [Checksum]
+    private void sendRequest(byte id, byte[] payload) {
+        byte[] requestData = new byte[payload == null ? 2 : (payload.length + 4)];
+
+        requestData[0] = START_FLAG;
+        requestData[1] = id;
+        if (payload != null) {
+            if (payload.length > 255) {
+                throw new IllegalArgumentException("Payload too large");
+            }
+
+            byte payloadLength = (byte) payload.length;
+            requestData[2] = payloadLength;
+
+            System.arraycopy(payload, 0, requestData, 3, payload.length);
+
+            byte checksum = 0;
+            for (int i = 0; i < payloadLength - 1; i++) {
+                checksum ^= requestData[i];
+            }
+            requestData[requestData.length - 1] = checksum;
+        }
+
+        int written = port.writeBytes(requestData, requestData.length);
+        if (written != requestData.length) {
+            throw new RuntimeException("Failed to write all data: " + written);
+        }
+        System.out.println("Wrote data");
+
+        System.out.println(toHex(requestData, requestData.length));
+    }
+
+    @Override
+    public int getListeningEvents() {
+        return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
+    }
+
+    @Override
+    public void serialEvent(SerialPortEvent event) {
+        byte[] data = new byte[port.bytesAvailable()];
+        int read = port.readBytes(data, data.length);
+
+        System.out.println("Hex: " + toHex(data, read));
+    }
+
+    private String toHex(byte[] data, int limit) {
+        final char[] CHARS = "0123456789abcdef".toCharArray();
+        char[] hex = new char[limit * 2];
+
+        for (int i = 0; i < limit; i++) {
+            int hi = (data[i] & 0xF0) >>> 4;
+            int lo = data[i] & 0x0F;
+
+            hex[i * 2] = CHARS[hi];
+            hex[i * 2 + 1] = CHARS[lo];
+        }
+
+        return String.valueOf(hex);
     }
 }
--- Lidar/src/main/java/frc/lidar/task/LidarMain.java
+++ Lidar/src/main/java/frc/lidar/task/LidarMain.java
@@ -1,24 +1,12 @@
 package frc.lidar.task;
 
-import com.fazecast.jSerialComm.SerialPort;
+import frc.lidar.lib.Lidar;
 
 public class LidarMain {
-    public static void main(String[] args) {
-        SerialPort lidarPort = null;
-        System.out.println("Available serial ports:");
-        for (SerialPort port : SerialPort.getCommPorts()) {
-            System.out.println(port.getSystemPortName() + " [" + port.getDescriptivePortName() + "]: " + port.getPortDescription());
+    public static void main(String[] args) throws Exception {
+        Lidar lidar = new Lidar();
+        lidar.startScanning();
 
-            if (port.getPortDescription().contains("CP2102")) {
-                if (lidarPort != null) {
-                    throw new RuntimeException("Port conflict, are multiple lidars connected?");
-                }
-                lidarPort = port;
-            }
-        }
-        if (lidarPort == null) {
-            throw new RuntimeException("Could not find lidar");
-        }
-        System.out.println("Found lidar on port " + lidarPort.getSystemPortName());
+        Thread.sleep(10000);
     }
 }
