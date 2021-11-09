4c4c8e4550aec8d56edea7b9551635bfee32e763
mvog2501 <91571536+mvog2501@users.noreply.github.com>
2021-11-09 17:45:06 -0600

    Start fresh.

--- VisionProcessing/OpenCV/TestJava.java
+++
@@ -1,6 +0,0 @@
-public class TestJava {
-
-public static void main(String[] args) {
-System.out.print("Hello world " + Math.PI); 
-}
-}
--- VisionProcessing/OpenCV/testingReadingSkills.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-img = cv2.imread("C:/Users/Laptop 5/Documents/trainingData/maybe/Lenna.png")
-
-
-cv2.imshow("Image", img)
-cv2.waitKey(0)
