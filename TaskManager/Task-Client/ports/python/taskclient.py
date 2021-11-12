c99c85c581e8a95d58b14efbeb0419762d4af5d4
Ryan Heuer <63077980+rmheuer@users.noreply.github.com>
2021-11-12 12:22:47 -0600

    Create python TaskClient outline

---
+++ TaskManager/Task-Client/ports/python/taskclient.py
@@ -0,0 +1,15 @@
+class TaskMessenger:
+    def __init__(self, messageServerHost, messageServerPort, name):
+        pass
+
+    def sendMessage(mType, data):
+        pass
+
+    def read():
+        pass
+
+    def disconnect():
+        pass
+
+    def setMessageCallback(callback):
+        pass
