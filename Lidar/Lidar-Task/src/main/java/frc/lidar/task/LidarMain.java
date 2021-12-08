93ff3369c53918b870fadce328f7620ce94f5db5
Ryan Heuer <63077980+rmheuer@users.noreply.github.com>
2021-12-08 12:54:31 -0600

    Create lidar task (untested)

--- Lidar/Lidar-Task/src/main/java/frc/lidar/task/LidarMain.java
+++ Lidar/Lidar-Task/src/main/java/frc/lidar/task/LidarMain.java
@@ -1,36 +1,80 @@
 package frc.lidar.task;
 
 import frc.lidar.lib.Lidar;
+import frc.lidar.lib.LidarHealth;
+import frc.lidar.lib.ScanEntry;
+import frc.taskmanager.taskclient.TaskMessenger;
 
-import java.util.Scanner;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LidarMain {
     public static void main(String[] args) {
         Lidar lidar = new Lidar();
+        TaskMessenger msg = new TaskMessenger("localhost", 8264);
+
+        System.out.println("Getting health of lidar");
         lidar.getHealth().thenAccept((health) -> {
-            System.out.println("H " + health);
+            System.out.println("Lidar health is " + health);
+            if (health == LidarHealth.ERROR) {
+                System.err.println("Lidar is not working!");
+                System.exit(-1);
+            }
+
+            msg.sendMessage("Ready", new byte[0]);
         });
 
-        lidar.setScanStartCallback(() -> System.out.println("S " + System.currentTimeMillis()));
+        List<ScanEntry> scanData = new ArrayList<>();
         lidar.setScanDataCallback((entry) -> {
-            if (entry.getDistance() == 0) return;
-            if (entry.getQuality() == 0) return;
-
-            System.out.println("s " + entry.getQuality() + " " + entry.getDistance() + " " + entry.getAngle());
+            synchronized (scanData) {
+                scanData.add(entry);
+            }
+        });
+        lidar.setScanStartCallback(() -> {
+            ByteArrayOutputStream b = new ByteArrayOutputStream();
+            DataOutputStream out = new DataOutputStream(b);
+            synchronized (scanData) {
+                out.writeInt(scanData.size());
+                for (ScanEntry entry : scanData) {
+                    out.writeInt(entry.getQuality());
+                    out.writeDouble(entry.getAngle());
+                    out.writeDouble(entry.getDistance());
+                }
+                scanData.clear();
+            }
+            msg.sendMessage("Scan", b.toByteArray());
         });
 
-        try {
-            Thread.sleep(1000);
-        } catch (Exception e) {}
-
-        System.out.println("M The lidar is starting");
-        lidar.startScanning();
+        boolean scanning = false;
+        msg.setMessageCallback((name, data) -> {
+            switch (name) {
+                case "Start":
+                    if (!scanning) {
+                        lidar.startScanning();
+                    }
+                    break;
+                case "Stop":
+                    if (scanning) {
+                        lidar.stopScanning();
+                    }
+                    break;
+                default:
+                    System.out.println("Warning: Unknown message received: " + name);
+                    break;
+            }
+        });
 
         // Run forever
         while (true) {
+            msg.read(); // Read incoming messages
+
             try {
-                Thread.sleep(1000000);
-            } catch (Exception e) {}
+                Thread.sleep(25);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
