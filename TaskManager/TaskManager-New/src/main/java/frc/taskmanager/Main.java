de1adaee1617c6e1fc87c58f33608d99f997b7d8
mvog2501 <63077980+rmheuer@users.noreply.github.com>
2021-12-12 18:46:16 -0600

    Add MessengerClient to TaskManager-New

---
+++ TaskManager/TaskManager-New/src/main/java/frc/taskmanager/Main.java
@@ -0,0 +1,11 @@
+package frc.taskmanager;
+
+public final class Main {
+    public static void main(String[] args) {
+        System.out.println("TaskManager starting");
+    }
+
+    private Main() {
+        throw new AssertionError();
+    }
+}
