0af331788fc12a9452749ed42b4b663698b5d6b3
Mason Vogt <91571536+mvog2501@users.noreply.github.com>
2021-12-18 12:50:27 -0600

    Window outputs for same cameras are now stacked

--- Vision/Vision_Processing.py
+++ Vision/Vision_Processing.py
@@ -210,7 +210,8 @@ class Vision:
             # cv2.imshow("Result " + str(cameraNumber) + " Instance " + str(self.instanceNumber),frameResult)
             # cv2.imshow("Binary " + str(cameraNumber) + " Instance " + str(self.instanceNumber),binary)
             binary3 = cv2.cvtColor(binary,cv2.COLOR_GRAY2BGR)
-            cv2.imshow("Binary3",binary3)
+            stacked = np.hstack((binary3,frameResult))
+            cv2.imshow("Camera " + str(cameraNumber) + " Instance " + str(self.instanceNumber),stacked)
 
 
         return(angleX,angleY,angle2X)
