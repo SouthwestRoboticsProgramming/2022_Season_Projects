c4ca41331348e814c1d152783784ea9920f8b5c0
Mason Vogt <91571536+mvog2501@users.noreply.github.com>
2022-01-03 18:44:29 -0600

    Started Implementing MK4 Swerve

--- SwerveRobot/src/main/java/frc/robot/drive/SwerveDrive.java
+++ SwerveRobot/src/main/java/frc/robot/drive/SwerveDrive.java
@@ -1,6 +1,9 @@
 package frc.robot.drive;
 
 import static frc.robot.Constants.*;
+import edu.wpi.first.wpilibj.kinematics.chassisSpeeds;
+import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.wpilibj.kinematics.SwereveModuleStates;
 
 public class SwerveDrive {
     /*
@@ -13,12 +16,14 @@ public class SwerveDrive {
      * w4 ------- w3
      */
     private final SwerveModule w1, w2, w3, w4;
+    private final ChassisSpeeds chassisSpeeds;
 
     public SwerveDrive() {
         w1 = new SwerveModule(DRIVE_PORT_1, TURN_PORT_1, CAN_PORT_1);
         w2 = new SwerveModule(DRIVE_PORT_2, TURN_PORT_2, CAN_PORT_2);
         w3 = new SwerveModule(DRIVE_PORT_3, TURN_PORT_3, CAN_PORT_3);
         w4 = new SwerveModule(DRIVE_PORT_4, TURN_PORT_4, CAN_PORT_4);
+        chassisSpeeds = new ChassisSpeeds(0.0, 0.0, 0.0);
     }
 
     public void setWheelTargetAngle(double angle) {
@@ -43,10 +48,19 @@ public class SwerveDrive {
     }
 
     public void update() {
-        w1.update();
-        w2.update();
-        w3.update();
-        w4.update();
+        SwereveModuleStates[] states = kinematics.toSwereveModuleStates(chassisSpeeds);
+        w2.set(states[0].speedMetersPerSecond / MAX_VELOCITY * MAX_VOLTAGE, states[0].angle.getRadians());
+        w3.set(states[1].speedMetersPerSecond / MAX_VELOCITY * MAX_VOLTAGE, states[1].angle.getRadians());
+        w4.set(states[2].speedMetersPerSecond / MAX_VELOCITY * MAX_VOLTAGE, states[2].angle.getRadians());
+        w1.set(states[3].speedMetersPerSecond / MAX_VELOCITY * MAX_VOLTAGE, states[3].angle.getRadians());
+
+
+
+        // w1.update();
+        // w2.update();
+        // w3.update();
+        // w4.update();
+
     }
 
     public void disable() {
