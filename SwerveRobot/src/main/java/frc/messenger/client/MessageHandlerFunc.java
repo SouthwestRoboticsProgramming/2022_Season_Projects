e125ae2f008cc0194e94455f441c033fe488f235
rmheuer <63077980+rmheuer@users.noreply.github.com>
2022-01-31 12:27:16 -0600

    Message handlers

---
+++ SwerveRobot/src/main/java/frc/messenger/client/MessageHandlerFunc.java
@@ -0,0 +1,11 @@
+package frc.messenger.client;
+
+import java.io.DataInputStream;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+@FunctionalInterface
+public interface MessageHandlerFunc {
+    void handleMessage(String type, DataInputStream in) throws IOException;
+}
