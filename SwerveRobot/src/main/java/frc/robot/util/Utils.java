366b5932cd3a610bcaa034a0e8e9810f176f98d6
Mason Vogt <91571536+mvog2501@users.noreply.github.com>
2022-02-03 13:16:23 -0600

    I did an oops

--- SwerveRobot/src/main/java/frc/robot/util/Utils.java
+++ SwerveRobot/src/main/java/frc/robot/util/Utils.java
@@ -19,7 +19,12 @@ public final class Utils {
         return -Math.PI + ((Math.PI * 2 + ((angle + Math.PI) % (Math.PI * 2))) % (Math.PI * 2));
     }
 
-    // Makes the angle be in the interval (Equivalent) [-180)
+
+    public static double normalizeAngleDegrees(double angle) {
+        return -180 + ((360 + ((angle + 180) % 360)) % 360);
+    }
+
+    // Makes the angle be in the interval (Equivalent) [-180,180)
     public static Rotation2d normalizeRotation2d(Rotation2d angle) {
         return new Rotation2d(-180 + ((180 * 2 + ((angle.getDegrees() + 180) % (180 * 2))) % (180 * 2)));
     }
