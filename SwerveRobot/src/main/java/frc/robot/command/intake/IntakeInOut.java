ce579fefa3b9eec9c018fe61638b1d8cd4fd8212
Ryan Heuer <63077980+rmheuer@users.noreply.github.com>
2022-02-18 12:06:38 -0600

    Update intake command

--- SwerveRobot/src/main/java/frc/robot/command/intake/IntakeInOut.java
+++ SwerveRobot/src/main/java/frc/robot/command/intake/IntakeInOut.java
@@ -7,50 +7,30 @@ package frc.robot.command.intake;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.command.Command;
 
 import static frc.robot.constants.IntakeConstants.*;
 
-public class IntakeInOut extends CommandBase {
+public class IntakeInOut extends Command {
   private final TalonSRX motor;
 
-  private int time = 0;
+  private int timer = 0;
   private boolean out;
   private boolean done = false;
-  /** Creates a new intakeUp. */
-  public IntakeInOut(TalonSRX motor) {
-    // Use addRequirements() here to declare subsystem dependencies.
-    this.motor = motor;
-  }
 
-  public void setOut(boolean out) {
+  public IntakeInOut(TalonSRX motor, boolean out) {
+    this.motor = motor;
     this.out = out;
   }
 
-  // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    while (time < INTAKE_TIME) {
-      if (out) {
-        motor.set(ControlMode.PercentOutput,-0.15);
-      } else {
-        motor.set(ControlMode.PercentOutput,0.15);
-      }
+  public boolean run() {
+    if (out) {
+      motor.set(ControlMode.PercentOutput, -0.15);
+    } else {
+      motor.set(ControlMode.PercentOutput, 0.15);
     }
-    done = true;
-  }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return done;
+    return ++timer >= INTAKE_TIME;
   }
 }
